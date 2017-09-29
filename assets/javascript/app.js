345386475060e304e5f232a9ff2dd3c838749056
Nate Morgan <natmor88@gmail.com>
2017-09-29 16:48:27 -0400

    some work done

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$("#1").on("click", function() {
+	alert("You clicked 1")
+})
+
+$("#2").on("click", function() {
+	alert("You clicked 2")
+})
+
+$("#3").on("click", function() {
+	alert("You clicked 3")
+})
+
+$("#4").on("click", function() {
+	alert("You clicked 4")
+})
